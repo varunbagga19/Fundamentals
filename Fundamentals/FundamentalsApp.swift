c75e433ff8a59734b58847384a4d9675fd96fc4f
varunbagga19 <varunbagga19@gmail.com>
2023-09-28 09:32:45 +0530

    Initial Commit

---
+++ Fundamentals/FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundamentalsApp.swift
+//  Fundamentals
+//
+//  Created by Varun Bagga on 28/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
